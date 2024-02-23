b63670b051ac96130e08fa2ccea88121260ddb30
RonanKer29 <kervella.ronan@hotmail.com>
2024-02-23 11:08:36 +0100

    Add dotenv-rails

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,9 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "lists#index"
   # resources :movies
+
   resources :lists do
     resources :bookmarks, only: [:new, :create]
   end
